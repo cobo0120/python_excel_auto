9a886aa4a52e551ff93c7bb6b8bfcc0c6a7cafc5
cobo0120 <cobo94251@gmail.com>
2024-01-27 14:09:51 +0900

    2024012705

--- webapplication_auto_login.py
+++ webapplication_auto_login.py
@@ -1,4 +1,5 @@
 # 必要なライブラリをインポート
+import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,19 +15,19 @@ import glob
 # フォルダに保存されている指定のエクセルファイルを参照する
 
 
-def list_files_recursively(directory):
-    return glob.glob(os.path.join(directory, '**/*.xlsx'), recursive=True)
+# def list_files_recursively(directory):
+#     return glob.glob(os.path.join(directory, '**/*.xlsx'), recursive=True)
 
 
-path = '在庫管理表'
-excel_files = list_files_recursively(path)
+# path = '/Users/kouheitakahashi/excel_auto/在庫管理表/在庫リスト.xlsx'
+# excel_files = list_files_recursively(path)
 
-# すべてのExcelファイルを別々のデータフレームにロード
-all_dfs = [pd.read_excel(file) for file in excel_files]
+# # すべてのExcelファイルを別々のデータフレームにロード
+# all_dfs = [pd.read_excel(file) for file in excel_files]
 
-# # すべてのデータフレームを一つに結合
-combined_df = pd.concat(all_dfs, ignore_index=True)
-print(combined_df)
+# # # すべてのデータフレームを一つに結合
+# combined_df = pd.concat(all_dfs, ignore_index=True)
+# print(combined_df)
 
 # chromeブラウザ起動オプションを指定
 chrome_options = webdriver.ChromeOptions()
@@ -94,19 +95,41 @@ link.click()
 
 time.sleep(10)
 # 1/18レッスンはここまで
+# 新規申請まで表示できた
 
 
 # 1/27のレッスンで実現したいこと
+# 承認画面のボタン操作までの流れを実装
 # # openpyexlを使っていく
-# １.フォルダ内にある在庫管理表にアクセス
+# １.指定フォルダ内にある在庫管理表にアクセス
+
+# Excelファイルへのパスを指定
+path = "/Users/kouheitakahashi/excel_auto/在庫管理表/在庫リスト.xlsx"
+# ワークブックとアクティブなシートを開く
+wb_obj = openpyxl.load_workbook(path)
+sheet_obj = wb_obj.active
 
 
 # 2.再発注用のフラグが立っているもの（B列）を参照する
 # （本来であればフラグを立っているものは全て申請するが今回は4行目のみ）
+# B列の4行目のセルを取得
+cell_obj = sheet_obj.cell(row=4, column=2)
+# セルの値を出力
+print(cell_obj.value)
 
-
-# 3.アクセス後４行目を入力できるように設計する
+# 3.アクセス後４行目をWEB申請画面の新規申請に入力できるように設計する
 # （エクセルを参照して入力する動きを見たいので少しの設定で１行入力で）
+# 新しいデータを別の入力欄に入力
+# 例えば、E列の4行目に入力する場合
+sheet_obj.cell(row=4, column=5).value = cell_obj.value  # E列は5番目の列なのでcolumn=5
 
 
 # 4.承認ボタンを押し、承認する動作まで進める（申請できないが今回はそれで）
+# 指定されたXPathに一致するボタンを見つけてクリック
+button1 = driver.find_element_by_xpath(
+    '//*[@id="app"]/main/div[1]/form/button[2]')
+button1.click()
+
+button2 = driver.find_element_by_xpath(
+    '//*[@id="exampleModal"]/div/div/div[3]/button[1]')
+button2.click()
